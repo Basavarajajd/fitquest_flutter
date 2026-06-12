90bf0814153ab9d4fc8316516395389fc5500675
Basavarajajd <basavarajakdb@gmail.com>
2026-06-12 13:04:30 +0530

    Fix pedometer stub and upgrade AGP/Kotlin versions

--- lib/core/services/step_service.dart
+++ lib/core/services/step_service.dart
@@ -1,33 +1,10 @@
 import 'dart:async';
-import 'package:pedometer/pedometer.dart';
 
 class StepService {
-  StreamSubscription<StepCount>? _subscription;
+  StreamSubscription? _subscription;
 
-  final _stepController = StreamController<int>.broadcast();
-  Stream<int> get stepStream => _stepController.stream;
-
-  int _initialSteps = -1;
-
-  void startListening() {
-    _subscription = Pedometer.stepCountStream.listen(
-      (StepCount event) {
-        if (_initialSteps == -1) {
-          _initialSteps = event.steps;
-        }
-
-        final currentSteps = event.steps - _initialSteps;
-        _stepController.sink.add(currentSteps < 0 ? 0 : currentSteps);
-      },
-    );
-  }
-
-  void stopListening() {
+  void start() {}
+  void stop() {
     _subscription?.cancel();
-  }
-
-  void dispose() {
-    _subscription?.cancel();
-    _stepController.close();
   }
 }
